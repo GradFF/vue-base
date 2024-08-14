f805588f4afbc6cb9f1b649224be825abf9c87ea
mrfneto <mrfneto@gmail.com>
2024-08-14 18:13:18 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="container mx-auto">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useStore } from '@/stores'
+
+const { user } = useStore()
+
+console.log(user)
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <h1>Home Page</h1>
+    <p class="mt-4 text-gray-600">{{ user?.name }}</p>
+  </div>
+</template>
+@/stores
